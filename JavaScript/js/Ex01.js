390ce2ac67477062df2e4ede77a4fc1a4736de06
MarcoRocha13 <88690366+MarcoRocha13@users.noreply.github.com>
2022-02-02 10:01:45 -0300

    Atualização

---
+++ JavaScript/js/Ex01.js
@@ -0,0 +1,11 @@
+
+var c=prompt("Digite a temperatura em Celcius: ");
+alert(c)
+var f=c*1.8+32
+var k=273+c
+alert("A temperatura em Celicius é "+c)
+alert("A temperatura em kelvin é "+k)
+alert("A temperatura em Fahrenheit é "+f)
+
+
+
